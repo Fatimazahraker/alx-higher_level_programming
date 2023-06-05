42a3aed45dba74c7a445daf1bd8e2c9941c3f878
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-06-05 13:37:06 -0700

    task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check in a list have a loop
+ * @list: list
+ * Return: 1 is success or 0 
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *chain1, *chain2;
+
+	if (!list)
+	{
+		return (0);
+	}
+	chain1 = list;
+	chain2 = list->next;
+	while (chain1 && chain2 && chain2->next)
+	{
+		if (chain1 == chain2)
+		{
+			return (1);
+		}
+		chain1 = chain1->next;
+		chain2 = chain2->chain2->next;
+	}
+	return (0);
+}
