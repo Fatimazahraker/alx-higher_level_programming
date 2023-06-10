899fbe851b717088f48cae87f3671579e7328bee
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-06-10 12:24:22 -0700

    task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     len = len(argv)
     if len == 1:
-        print("0 arguments.")
+        print(0)
     elif len > 1:
-        print("{:d} argument{:s}".format(len-1, "s:" if len > 2 else ":"))
+        total = 0
         for i in range(1, len):
-            print("{:d}: {:s}".format(i, argv[i]))
+            total += int(argv[i])
+        print("{:d}".format(total))
