c90d5528413a0bb68d32bc4fdd59be471d72515c
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-06-14 14:14:20 -0700

    task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -11,7 +11,7 @@ def roman_to_int(roman_string):
             "C": 100,
             "D": 500,
             "M": 1000
-    }
+            }
     num = 0
 
     for i in range(len(roman_string)):
@@ -19,8 +19,8 @@ def roman_to_int(roman_string):
             return (0)
 
         if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+            num += roman_dict[roman_string[i]] * -1
 
         else:
             num += roman_dict[roman_string[i]]
