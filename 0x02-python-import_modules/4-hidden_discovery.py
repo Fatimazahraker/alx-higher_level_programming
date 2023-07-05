f056dad1ea171044277b9329ecf6ffb4d7441264
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-07-05 08:32:55 -0700

    task

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!usr/usr/bin/python3
 if __name == "__main__":
     import hidden_4
-    for i in range(len(dir(hidden))):
-        if (dir(hidden_4)[i][:2] == "__"):
-            continue
-        print(dir(hidden_4)[i])
+    for word in (dir(hidden_4):
+            if word[1] != '_':
+            print(word)
