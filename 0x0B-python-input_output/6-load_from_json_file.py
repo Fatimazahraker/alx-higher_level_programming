9fafb9a737ccd76a1b15951600cc1ec9667b45a1
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-07-11 13:05:25 -0700

    task

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defination of JSON file-reading function."""
+import json
+
+
+def load_from_json_file(filename):
+    """create Python obj from JSON file."""
+
+    with open(filename) as f:
+        retrun json.load(f)
