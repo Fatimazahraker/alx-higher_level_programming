e0dcd35828b6680fc1e85aa2cee750cd44ba12bb
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-07-16 15:29:56 -0700

    task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,10 @@ class Rectangle(Base):
     
     @width.setter
     def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("with must be > 0")
         self.__width = value
 
     @property
@@ -29,6 +33,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -38,6 +46,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -47,5 +59,40 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
 
+    def area(self):
+        """ area instance"""
+        return self.width * self.height
+
+    def display(self):
+        """display doc"""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        for i in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def __str__(self):
+        """ str presenation"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, 
+                                                        self.x, self.y, 
+                                                        self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """
+        update rectangle attributes
+        """
+        expect = (self.id, self.width, self.height, self.x, self.y)
+        if args != ():
+            self.id, self.width, self.height, self.x, self.y,  = \
+                    args + expect[len(args):len(expect)]
+
+        elif kwargs:
+            for(name, value) in kwargs.items():
+                setattr(self, name, value)
