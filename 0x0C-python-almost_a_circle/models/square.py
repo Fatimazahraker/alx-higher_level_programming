b5b8a6ed176c2abb2dc82e89ea695b7954299e2d
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-07-17 13:21:27 -0700

    task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
     @property
     def size(self):
         """ doc """
-        return self.height
+        return self.width
 
     @size.setter
     def size(self, value):
