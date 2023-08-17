9715136c805c7a3a24b4d9c9e0a3849ec3a76efd
Fatimazahraker <fatimazahraker>
2023-08-17 20:32:44 +0000

    task

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,11 +1,7 @@
--- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
--- cat 6-states.sql | sudo mysql -hlocalhost -uroot -p
--- echo 'INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");' | sudo mysql -hlocalhost -uroot -p hbtn_0d_usa
--- echo 'SELECT * FROM states;' | sudo mysql -hlocalhost -uroot -p hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa
-CREATE TABLE
-    IF NOT EXISTS states (
-	        id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
-		        name VARCHAR(256)
-			    )
+-- creates table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	    PRIMARY KEY(`id`),
+	    `id`   INT          NOT NULL AUTO_INCREMENT,
+	    `name` VARCHAR(256) NOT NULL
+	);
