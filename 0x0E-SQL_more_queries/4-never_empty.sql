8d6789ef0b23ee0efbadd87f5341b62b634972ee
Fatimazahraker <fatimazahraker>
2023-08-17 20:30:42 +0000

    task

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
---echo 'INSERT INTO id_not_null (name) VALUES ("Best");' | sudo mysql -hlocalhost -uroot -p hbtn_0d_2
+-- echo 'INSERT INTO id_not_null (name) VALUES ("Best");' | sudo mysql -hlocalhost -uroot -p hbtn_0d_2
 CREATE TABLE
     IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256))
