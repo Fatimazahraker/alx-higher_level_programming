a7a7e86089076ff3f3238d387879fc0dccef5002
Fatimazahraker <fatimazahraker>
2023-08-17 20:31:20 +0000

    task

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
---echo 'SELECT * FROM unique_id;' | sudo mysql -hlocalhost -uroot -p hbtn_0d_2
+-- echo 'SELECT * FROM unique_id;' | sudo mysql -hlocalhost -uroot -p hbtn_0d_2
 CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256)
