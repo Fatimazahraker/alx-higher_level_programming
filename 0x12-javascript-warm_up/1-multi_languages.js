bec701d4d9acf7724d4647f20dbc44b93c64b240
Fatimazahraker <fatimazahraker>
2023-09-12 20:24:17 +0000

    task

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVariable = 'C is fun';
+const myVariable2 = 'Python is cool';
+const myVariable3 = 'JavaScript is amazing';
+console.log(myVariable);
+console.log(myVariable2);
+console.log(myVariable3);
