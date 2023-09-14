6232e80cb857a80cf4b8882e59ae986398d53372
Fatimazahraker <fatimazahraker>
2023-09-14 13:12:27 +0000

    task

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    let len = list.length - 1;
-    let m = 0;
-    while ((len - m) > 0) {
-      const tmp = list[len];
-      list[len] = list[m];
-      list[m] = tmp;
-      m++;
-      len--;
-    }
-    return list;
+  let len = list.length - 1;
+  let m = 0;
+  while ((len - m) > 0) {
+    const tmp = list[len];
+    list[len] = list[m];
+    list[m] = tmp;
+    m++;
+    len--;
+  }
+  return list;
 };
