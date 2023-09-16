c3c50b4bae1ddfcb737a187edb3df16d89796ab8
Fatimazahraker <fatimazahraker>
2023-09-16 19:36:09 +0000

    tas

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     """ func not run when imported as module """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
-    cur i= db.cursor()
-    query = """SELECT cities.id, cities.name, states.name
-    FROM cities INNER JOIN states
-    ON cities.state_id = states.id
-    ORDER BY cities.id""")
+    cur = db.cursor()
+    query = "SELECT cities.id, cities.name, states.name \
+            FROM cities INNER JOIN states \
+            ON cities.state_id = states.id \
+            ORDER BY cities.id"
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
