ea06d22b53f69a593ec5010afe27946f55a1bc69
Fatimazahraker <fatimazahraker>
2023-09-16 14:09:45 +0000

    task

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" module filter state Db with safe from SQL injection """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """ func not run when imported as module """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+    start = db.cursor()
+    query = """SELECT cities.id, cities.name, states.name
+                      FROM cities INNER JOIN states
+                                        ON cities.state_id = states.id
+                                                          ORDER BY cities.id""")
+    start.execute(query, (argv[4],))
+    rows = start.fetchall()
+    for row in rows:
+        print(row)
