25c0ff272c41e8a74c88d7d0ee1fd20e7de9d54a
Fatimazahraker <fatimazahraker>
2023-09-17 20:37:27 +0000

    task

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+""" lists all State objects from the database hbtn_0e_6_usa
 """
+
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -8,11 +9,12 @@ from sqlalchemy import create_engine, select, insert
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                sys.argv[1], sys.argv[2], sys.argv[3]
+                ),
+                 pool_pre_ping=True,
+                 )
+
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -21,4 +23,5 @@ if __name__ == "__main__":
     if updateState:
         updateState.name = 'New Mexico'
         session.commit()
+
     session.close()
