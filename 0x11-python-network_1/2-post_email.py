9a71ec0d72831a64c8428cb38aef04e260f2a23b
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-09-29 12:40:59 -0700

    task

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL"""
+
+import urllib.request
+import sys
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('utf-8')
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print(body.read().decode('utf-8'))
