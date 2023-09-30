367cd2f9dca81d9089663c7fab0e091280a4b519
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-09-30 03:36:16 -0700

    task

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email address, sends a POST request"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = sys.argv[2]
+    r = requests.post(url, params= {"email": data})
+    print("Your email is: {data}")
