3578fd548dabd77005467f461d5b8455078b68b2
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-01 13:08:21 -0700

    task

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+~#!/usr/bin/python3
 """script that takes in a URL and an email address, sends a POST request"""
 
 import requests
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {"email": email}
-    r = requests.post(url, params=data)
-    print("Your email is: {}".format(r.text))
+    r = requests.post(url, data=data)
+    print(r.text)
