0ef9145c06a1234a4b46d17095f3e47ffcf09cd9
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-25 13:39:34 -0700

    task

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+const content = process.argv[3];
+fs.writeFile(process.argv[2], content, err => {
   if (err) {
     console.error(err);
-    return;
   }
-  console.log(data);
 });
